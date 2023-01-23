af99959a84b78c0c249a5b6055300bf3db435536
Ryan Faiz Sanie <ryanfaiz2005@gmail.com>
2023-01-23 07:15:13 +0700

    Add navbar in index.php

--- index.php
+++ index.php
@@ -7,6 +7,19 @@
     <title>Home | DigiLib</title>
 </head>
 <body>
-    
+    <nav>
+        <a href=""><h1>DigiLib</h1></a>
+
+        <div>
+            <a href=""><p>Collection</p></a>
+            <a href=""><p>Genre</p></a>
+            <a href=""><p>Categories</p></a>
+        </div>
+
+        <div>
+            <a href=""><img src="" alt=""></a>
+            <a href=""><img src="" alt=""></a>
+        </div>
+    </nav>
 </body>
 </html>
