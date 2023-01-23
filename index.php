0cfb21729948303f3a1ac92efc098f18e542bb46
Ryan Faiz Sanie <ryanfaiz2005@gmail.com>
2023-01-23 17:44:14 +0700

    Add more sample for book display

--- index.php
+++ index.php
@@ -42,6 +42,56 @@
                     <h2>Book Title</h2>
                 </div>
             </a>
+
+            <a href="">
+                <div class="book">
+                    <div class="book-cover">
+                        <img src="/img/sample/book-cover-sample.webp" alt="Book Cover 2">
+                    </div>
+
+                    <h2>Book Title</h2>
+                </div>
+            </a>
+
+            <a href="">
+                <div class="book">
+                    <div class="book-cover">
+                        <img src="/img/sample/book-cover-sample.webp" alt="Book Cover 3">
+                    </div>
+
+                    <h2>Book Title</h2>
+                </div>
+            </a>
+
+            <a href="">
+                <div class="book">
+                    <div class="book-cover">
+                        <img src="/img/sample/book-cover-sample.webp" alt="Book Cover 4">
+                    </div>
+
+                    <h2>Book Title</h2>
+                </div>
+            </a>
+
+            <a href="">
+                <div class="book">
+                    <div class="book-cover">
+                        <img src="/img/sample/book-cover-sample.webp" alt="Book Cover 5">
+                    </div>
+
+                    <h2>Book Title</h2>
+                </div>
+            </a>
+
+            <a href="">
+                <div class="book">
+                    <div class="book-cover">
+                        <img src="/img/sample/book-cover-sample.webp" alt="Book Cover ">
+                    </div>
+
+                    <h2>Book Title</h2>
+                </div>
+            </a>
         </div>
     </section>
 </body>
