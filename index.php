22d02c606bc6379277a792d69d65448da904546d
Ryan Faiz Sanie <ryanfaiz2005@gmail.com>
2023-01-28 05:57:53 +0700

    Insert favicon in index.php

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home | DigiLib</title>
     <link rel="stylesheet" href="style/home.css">
+    <link rel="shortcut icon" href="/img/icon/favicon.webp" type="image/webp">
 </head>
 <body>
     <nav>
