480934438388917e8de1ba67501584cbcef2cfd9
Ryan Faiz Sanie <ryanfaiz2005@gmail.com>
2023-01-28 06:23:29 +0700

    Add some sample to booklist container

--- collection/index.php
+++ collection/index.php
@@ -27,7 +27,45 @@
     <section id="collection-sect">
         <h1>Collection</h1>
         <div id="booklist">
-            
+        <a href="">
+                    <div class="book">
+                        <div class="book-cover">
+                            <img src="/img/sample/book-cover-sample.webp" alt="Book Cover 1">
+                        </div>
+    
+                        <h2>Book Title</h2>
+                    </div>
+                </a>
+    
+                <a href="">
+                    <div class="book">
+                        <div class="book-cover">
+                            <img src="/img/sample/book-cover-sample.webp" alt="Book Cover 2">
+                        </div>
+    
+                        <h2>Book Title</h2>
+                    </div>
+                </a>
+    
+                <a href="">
+                    <div class="book">
+                        <div class="book-cover">
+                            <img src="/img/sample/book-cover-sample.webp" alt="Book Cover 3">
+                        </div>
+    
+                        <h2>Book Title</h2>
+                    </div>
+                </a>
+    
+                <a href="">
+                    <div class="book">
+                        <div class="book-cover">
+                            <img src="/img/sample/book-cover-sample.webp" alt="Book Cover 4">
+                        </div>
+    
+                        <h2>Book Title</h2>
+                    </div>
+                </a>
         </div>
     </section>
 </body>
