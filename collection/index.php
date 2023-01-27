5b4f75671c6c1fb6d7651adcaafafbe576190596
Ryan Faiz Sanie <ryanfaiz2005@gmail.com>
2023-01-28 06:18:11 +0700

    Add nav in collection page

--- collection/index.php
+++ collection/index.php
@@ -8,6 +8,21 @@
     <link rel="shortcut icon" href="/img/icon/favicon.webp" type="image/webp">
 </head>
 <body>
-    
+    <nav>
+        <a href="/"><h1>DigiLib</h1></a>
+
+        <div>
+            <a href="">Collection</a>
+            <a href="">Genre</a>
+            <a href="">Categories</a>
+        </div>
+
+        <div>
+            <a href=""><img src="/img/icon/search.webp" alt="Search Icon"></a>
+            <a href=""><img src="/img/icon/profile.webp" alt="Profile Icon"></a>
+        </div>
+    </nav>
+
+
 </body>
 </html>
