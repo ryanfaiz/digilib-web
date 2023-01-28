9f94ee3dabafd0c0955685e39864544d76e7cc6e
Ryan Faiz Sanie <ryanfaiz2005@gmail.com>
2023-01-28 13:39:06 +0700

    Link favicon and css file

--- categories/index.php
+++ categories/index.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Categories | DigiLib</title>
+    <link rel="shortcut icon" href="/img/icon/favicon.webp" type="image/webp">
+    <link rel="stylesheet" href="/style/categories.css">
 </head>
 <body>
     
