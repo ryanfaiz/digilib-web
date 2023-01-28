ac6f403d12f4e8c0cc0e8289ac4db84625bea008
Ryan Faiz Sanie <ryanfaiz2005@gmail.com>
2023-01-28 13:51:24 +0700

    Add <nav> in Search Page

--- search/index.php
+++ search/index.php
@@ -9,6 +9,21 @@
         <link rel="stylesheet" href="/style/search.css">
     </head>
     <body>
+    <nav>
+        <a href="/"><h1>DigiLib</h1></a>
+
+        <div>
+            <a href="/collection">Collection</a>
+            <a href="/tags">Tags</a>
+            <a href="/categories">Categories</a>
+        </div>
+
+        <div>
+            <a href="/search"><img src="/img/icon/search.webp" alt="Search Icon"></a>
+            <a href="/profile"><img src="/img/icon/profile.webp" alt="Profile Icon"></a>
+        </div>
+
         
+    </nav>
     </body>
     </html>
